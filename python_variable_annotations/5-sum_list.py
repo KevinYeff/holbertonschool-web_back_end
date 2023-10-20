cbef8d225dcb6fb64ba611c2aa6cb8fca208f428
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 23:21:19 -0500

    fix: List from typing imported incompatible types in assigment fixed(line 10)

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Task 5 complex types - list of floats"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """A type-annotated function that takes a list
     of floats as argument and returns their sum as
     a float"""
-    sum = 0
-    for float in input_list:
-        sum += float
+    sum: float = 0.0
+    for floats in input_list:
+        sum += floats
     return sum
