9e748a2cf2b6e92b09e1a3fe24c8bc8cc5ed7cef
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 22:39:44 -0500

    feat: task 2 Basic annotatons - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 2 Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """A type-annotated function that takes 1 argument
+    a float and returns the floor of it (int)"""
+    return int(n)
