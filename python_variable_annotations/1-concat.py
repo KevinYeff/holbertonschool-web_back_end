7f200516aa949b5d5da4e3ada04b7cf3b98b8f8e
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 22:13:57 -0500

    feat: task 1 Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 1 Basic annotation - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A type annotated function that takes 2
+    strings as arguments and specifies the returns
+    as a concatenated string"""
+    return str1 + str2
