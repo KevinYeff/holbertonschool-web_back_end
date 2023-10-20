660fa83f0a093a05bbf011cbc1d94d94c92be635
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 22:45:00 -0500

    feat: task3 Basic annotaton to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 3 Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """A type-annotaed function that takes a float as
+    argument and returns the string representation
+    of this argument"""
+    return str(n)
