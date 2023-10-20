f75cd3372db6d41f10cbd7574dc5b839d811acb7
KevinYeff <kevinyeff94@gmail.com>
2023-10-19 22:05:52 -0500

    feat: task 0 Basic annotation - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 0 Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """A type-annotated functions that recives
+    2 float numbers and specifies the return
+    as a float"""
+    return a + b
