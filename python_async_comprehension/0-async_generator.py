ee334718a8a8e7d744ac23b1014c061d49cab2be
KevinYeff <kevinyeff94@gmail.com>
2023-11-02 23:05:32 -0500

    feat: Coroutine function that waits and generates a radom float number

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Task 0 Async Generator"""
+
+import asyncio
+from typing import Generator
+from random import uniform
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Coroutine function that takes no arguments
+    The coroutine will loop 10 times, each time
+    asynchronously wait 1 second, the yield a random
+    number between 0 and 10
+    Generates: float numbers"""
+    for i in range(10):
+        # 2nd iteration Reanudes
+        # waits 1 sec per iteration
+        await asyncio.sleep(1)
+        # generates a random float number on demand
+        yield uniform(0,10)
+        # 1st iteration Pauses
