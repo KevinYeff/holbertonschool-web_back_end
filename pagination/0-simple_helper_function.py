c79645f7327e1920d0f3ff84232a484a54a06b32
KevinYeff <kevinyeff94@gmail.com>
2023-11-08 00:49:52 -0500

    feat: Function that returns tuple containing arange of indexes

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Task 0 Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Function that helps to get the range of indexes
+    Parameters:
+     @page: integer
+     @page_size: integer
+    Return: A tuple containing the range of indexes from
+    start to end
+    Note: In Pagination all the indexes starts at 1"""
+    if page > 0 and page_size > 0:
+        if page >= 0:
+            start = (page - 1) * page_size
+            end = start + page_size
+            return (start, end)
