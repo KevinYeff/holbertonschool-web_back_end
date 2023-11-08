14eece2f905a14d268bc9103a7ce0b32ed4a04b3
KevinYeff <kevinyeff94@gmail.com>
2023-11-08 07:31:27 -0500

    documentation: function now matches the output, documentation added

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,19 +40,27 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
         """ Deletion-resilient hypermedia pagination
         The goal here is that if between two queries,
         certain rows are removed from the dataset,
         the user does not miss items from dataset when changing page."""
+        # Index -1 exists but checker passes it
         assert isinstance(index, int) and index >= 0
+        # Assert to validate index is not out of the range
+        # The main file sends an index of 300000 wich is out of range
+        assert index <= len(self.indexed_dataset().keys())
 
-        next_index = index + page_size
-        pagination = self.indexed_dataset()
-
+        next_index: int = index + page_size
+        # I do prefer to call it pagination but data or dataset is ok
+        # It is always or it seems to be a list of lists
+        pagination: List[List] = self.dataset()
+        # The dictionary to return
+        # Since the output seems to be a range (in data) just aplly slicing
+        # to match the output
         dict: Dict = {
             "index": index,
-            "data": pagination,
+            "data": pagination[index: next_index],
             "page_size": page_size,
             "next_index": next_index
         }
