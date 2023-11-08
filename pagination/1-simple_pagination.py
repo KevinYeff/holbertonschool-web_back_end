8ee69bf97446c5244d7825fa67e1e9cf9b30469e
KevinYeff <kevinyeff94@gmail.com>
2023-11-08 02:39:37 -0500

    fix: file now matches pycodestyle requirements

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -27,35 +27,34 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Method that returns a page
-            Parameters:
-                @page: int starting page
-                @page_size: int size of page
-            Return: A list of the pagination"""
+        """Method that returns a page
+        Parameters:
+            @page: int starting page
+            @page_size: int size of page
+        Return: A list of the pagination"""
 
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            pagination: List[List] = self.dataset()
+        pagination: List[List] = self.dataset()
 
-            range_idx: Tuple = self.index_range(page, page_size)
+        range_idx: Tuple = self.index_range(page, page_size)
 
-            if range_idx[0] >= len(pagination):
-                return []
+        if range_idx[0] >= len(pagination):
+            return []
 
-            return (pagination[range_idx[0]:range_idx[1]])
-
+        return (pagination[range_idx[0]:range_idx[1]])
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-            """Function that helps to get the range of indexes
-            Parameters:
-                @page: integer
-                @page_size: integer
-                Return: A tuple containing the range of indexes from
-                start to end
-            Note: In Pagination all the indexes starts at 1"""
-            if page > 0 and page_size > 0:
-                pass
-            start = (page - 1) * page_size
-            end = start + page_size
-            return (start, end)
+        """Function that helps to get the range of indexes
+        Parameters:
+            @page: integer
+            @page_size: integer
+            Return: A tuple containing the range of indexes from
+            start to end
+        Note: In Pagination all the indexes starts at 1"""
+        if page > 0 and page_size > 0:
+            pass
+        start = (page - 1) * page_size
+        end = start + page_size
+        return (start, end)
