854e64a571aaf27a80a43e0234a7cb0b6aea9add
KevinYeff <kevinyeff94@gmail.com>
2023-11-11 13:58:57 -0500

    feat: Task 13 Iterating through report objects

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const array = [];
+  for (const key of Object.keys(report.allEmployees)) {
+    array.push(...report.allEmployees[key]);
+  };
+  return array;
+}
