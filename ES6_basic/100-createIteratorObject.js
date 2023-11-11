977d750e2a3942679a7ffc5596d3192169d318ee
KevinYeff <kevinyeff94@gmail.com>
2023-11-11 14:01:18 -0500

    fix: Line 5 unnecesary semicolon deleted to match eslint requirements

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -2,6 +2,6 @@ export default function createIteratorObject(report) {
   const array = [];
   for (const key of Object.keys(report.allEmployees)) {
     array.push(...report.allEmployees[key]);
-  };
+  }
   return array;
 }
