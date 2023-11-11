e1a6068bb75bd7a111764988802d8fbbced6fbc0
KevinYeff <kevinyeff94@gmail.com>
2023-11-11 15:40:52 -0500

    fix: several correcions added

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let str ='';
+  let str = '';
   let i = 0;
-  for(const name of reportWithIterator){
-    if (i < reportWithIterator.length - 1){
-      str += `${name} | `
+  for (const name of reportWithIterator) {
+    if (i < reportWithIterator.length - 1) {
+      str += `${name} | `;
     }
     else {
-      str += `${name}`
+      str += `${name}`;
     }
     i++;
   }
