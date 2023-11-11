5a4f61556fb92558697d755eeabeff6a103ce7c1
KevinYeff <kevinyeff94@gmail.com>
2023-11-11 15:37:01 -0500

    feat: Iterate through object

---
+++ ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let str ='';
+  let i = 0;
+  for(const name of reportWithIterator){
+    if (i < reportWithIterator.length - 1){
+      str += `${name} | `
+    }
+    else {
+      str += `${name}`
+    }
+    i++;
+  }
+  return str;
+}
