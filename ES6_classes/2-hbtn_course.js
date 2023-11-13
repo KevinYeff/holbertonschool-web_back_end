fbf7306db1e9caea0a9730cfb7dfe74d33e5b10a
KevinYeff <kevinyeff94@gmail.com>
2023-11-13 08:30:46 -0500

    test: first attemp

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      throw TypeError('ERROR');
+    } else if (students.every((el) => typeof el !== 'string')) {
+      throw TypeError('ERROR');
+    } else {
+      this._students = students;
+    }
+  }
+}
