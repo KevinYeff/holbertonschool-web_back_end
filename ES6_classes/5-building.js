9b7a5c3c9f96b201ac2e3628f904fb4e2b64abdd
KevinYeff <kevinyeff94@gmail.com>
2023-11-14 09:02:45 -0500

    test: Abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    } else {
+      this._sqft = sqft;
+    }
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
