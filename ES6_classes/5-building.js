f89bdc1ee648946e287bed509b841458d5fb261c
KevinYeff <kevinyeff94@gmail.com>
2023-11-14 09:24:11 -0500

    test: lint disable, file already matches the output

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building) {
+      this.evacuationWarningMessage();
+    }
   }
 
   get sqft() {
@@ -14,6 +17,7 @@ export default class Building {
       this._sqft = sqft;
     }
   }
+  /* eslint-disable */
 
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
