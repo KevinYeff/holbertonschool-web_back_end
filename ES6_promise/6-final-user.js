75113a9fe0db4e587aeae9251c8c75b0bb1a29f7
KevinYeff <kevinyeff94@gmail.com>
2023-11-17 18:10:08 -0500

    test: Barely matches the ... output

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,22 @@ import singUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([singUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([singUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((promisesResults) => {
+      for (const result of promisesResults) {
+        if (result.status === 'rejected') {
+          result.value = `${result.reason.name}: ${result.reason.message}`;
+        }
+      }
+      //     //console.log(result);
+      //   } else {
+      //     result.status = 'rejected'
+      //     // result.value = `${result.reason.name}: ${result.reason.message}`;
+      //     //  console.log(result);
+      //   }
+      // }
+      console.log(promisesResults);
+      return promisesResults;
+    })
+    .catch((error) => error);
 }
