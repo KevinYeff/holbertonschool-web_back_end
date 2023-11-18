e9316ec72093e2cd649821cf6874c631149bb558
KevinYeff <kevinyeff94@gmail.com>
2023-11-17 19:12:56 -0500

    fix: script now matches the test output

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,20 +4,22 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([singUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((promisesResults) => {
+      const formatedStructure = [];
       for (const result of promisesResults) {
         if (result.status === 'rejected') {
-          result.value = `${result.reason.name}: ${result.reason.message}`;
+          formatedStructure.push({
+            status: result.status,
+            value: `${result.reason.name}: ${result.reason.message}`,
+          });
+        } else {
+          formatedStructure.push({
+            status: result.status,
+            value: result.value,
+          });
         }
       }
-      //     //console.log(result);
-      //   } else {
-      //     result.status = 'rejected'
-      //     // result.value = `${result.reason.name}: ${result.reason.message}`;
-      //     //  console.log(result);
-      //   }
-      // }
-      //  console.log(promisesResults);
-      return promisesResults;
+      //  console.log(formatedStructure);
+      return formatedStructure;
     })
     .catch((error) => error);
 }
