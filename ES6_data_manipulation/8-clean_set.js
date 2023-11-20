bf0f41c8a6ea9741c2d0bba757e22976dca5ae6b
KevinYeff <kevinyeff94@gmail.com>
2023-11-20 12:45:37 -0500

    fix: Line 5 verification to make sure element is not undefined added

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
   const tempArray = [];
   for (const element of set) {
-    if (element.startsWith(startString)) {
+    if (element && element.startsWith(startString)) {
       tempArray.push(element.slice(startString.length));
     }
   }
