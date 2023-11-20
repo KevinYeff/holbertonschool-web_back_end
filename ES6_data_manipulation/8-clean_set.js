c9a09c9e3a5f000731fb3724646fad5a45dfbb1f
KevinYeff <kevinyeff94@gmail.com>
2023-11-20 12:28:23 -0500

    feat: task 8 Clean set Script that returns a formated string, based on coincidences inside a set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+  const tempArray = [];
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      tempArray.push(element.slice(startString.length));
+    }
+  }
+  return tempArray.join('-');
+}
