42c74f3df7912451b965e11140393003559a05d7
KevinYeff <kevinyeff94@gmail.com>
2023-11-19 21:45:01 -0500

    feat: Task 1 More mapping Script that uses map() method and reformats the given array of objects

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
 export default function getListStudentIds(array) {
   if (array instanceof Array) {
-    const newArray = [];
-    for (const obj of array) {
-      newArray.push(obj.id);
-    }
-    return newArray;
+    return array.map((obj) => (obj.id));
   }
   return [];
 }
